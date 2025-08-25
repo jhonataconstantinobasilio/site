4f988fe1fa8cb2ad1442db7a1c7217d4935d90e4
jhonataconstantinobasilio <jhonataconstantinobasilio@gmail.com>
2025-08-25 11:39:31 -0300

    Add files via upload

---
+++ progeto.py
@@ -0,0 +1,7 @@
+nome = input("digite seu nome por completo (letras maiuscula)")
+
+trabalho = input("digite com o que você trabalha ")
+
+motivo = input("Por qual motivo você está processando sua empresa ?")
+
+msg_sobre_seus_direitos = "clique abaixo para saber mais sobre seus direitos"
